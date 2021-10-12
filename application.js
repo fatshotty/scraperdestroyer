23f91b94488d0dca8b94858e36069dd7ecaf8485
fatshotty <fat@fatshotty.net>
2021-10-12 11:33:29 +0200

    restore application code

--- application.js
+++ application.js
@@ -1,17 +1,4 @@
 require('dotenv').config();
-const GlobalTunnel = require('global-tunnel-ng');
 
-
-// if ( process.env.HTTP_PROXY ) {
-//   GlobalTunnel.initialize({
-//     host: process.env.HTTP_PROXY,
-//     port: process.env.HTTP_PROXY_PORT,
-//     proxyAuth: process.env.HTTP_PROXY_AUTH // optional authentication
-//   });
-// }
-
-
-
-
-// require('./distruggere');
+require('./distruggere');
 require('./instascraper');
